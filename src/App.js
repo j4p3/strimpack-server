9929bf69f02c62b7eac06c452bfaeddffcb5af4d
JP <bonnerjp@gmail.com>
2018-04-11 16:00:02 -0400

    modal: use context api for modal state

--- src/App.js
+++ src/App.js
@@ -1,14 +1,55 @@
 import React, { Component } from 'react';
+import update from 'react-addons-update';
+
 import { StreamScreen } from './stream';
 import Nav from './nav';
+import { Modal, ModalContext, content } from './modal';
 import './app.css';
 
 class App extends Component {
+  constructor(props) {
+    super(props);
+
+    // 
+    // Context transformer methods
+    // 
+
+    this.close = () => {
+      this.setState((state) => {
+        return update(state, {
+          modal: { visible: { $set: false } }
+        });
+      });
+    };
+
+    this.subscribe = () => {
+      this.setState((state) => {
+          return update(state, { modal: {
+            visible: { $set: true },
+            content: { $set: content.subscribe }}
+        });
+      });
+    };
+
+    this.state = {
+      modal: {
+        content: content.hi,
+        visible: false,
+        hi: this.hi,
+        subscribe: this.subscribe,
+        close: this.close
+      }
+    };
+  }
+
   render() {
     return (
       <div className="root vertical container">
-        <Nav />
-        <StreamScreen />
+        <ModalContext.Provider value={this.state.modal}>
+          <Nav />
+          <StreamScreen />
+          <Modal />
+        </ModalContext.Provider>
       </div>
     );
   }
--- src/nav.js
+++ src/nav.js
@@ -1,4 +1,6 @@
 import React, { Component } from 'react';
+
+import { ModalContext } from './modal';
 import './nav.css';
 
 class Nav extends Component {
@@ -9,13 +11,23 @@ class Nav extends Component {
           <a className="logo-wrap" href="#"><li className="logo"></li></a>
           <a className="title" href="#"><li><span className="accent">Title</span></li></a>
         </ul>
-        <ul className="right">
-          <a href="https://www.strawpoll.me/15152682"><li><b>What's Next?</b></li></a>
-          <a href="#" className="trigger" data-modal="modal-sub"><li>Subscribe</li></a>
-          <a href="https://twitch.streamlabs.com/neuro"><li>Donate</li></a>
-          <a href="https://www.patreon.com/NeuroZerg"><li>Become a Patron</li> </a>
-          <a href="https://drive.google.com/file/d/165wFnuAV2bDpgwJr5RVy3EHRybkt7s1q/view?usp=sharing"><li>Replays</li> </a>
-        </ul>
+        <ModalContext.Consumer>
+          {(context) => {
+            return (
+              <ul className="right">
+                <a href="https://www.strawpoll.me/15152682"><li><b>What's Next?</b></li></a>
+                <a href="#"
+                   onClick={context.subscribe}><li>
+                Subscribe</li></a>
+                <a href="https://twitch.streamlabs.com/neuro"><li>
+                  Donate</li></a>
+                <a href="https://www.patreon.com/NeuroZerg"><li>
+                  Become a Patron</li> </a>
+                <a href="https://drive.google.com/file/d/165wFnuAV2bDpgwJr5RVy3EHRybkt7s1q/view?usp=sharing"><li>
+                  Replays</li> </a>
+              </ul>
+          )}}
+        </ModalContext.Consumer>
       </nav>
     );
   }
